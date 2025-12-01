01597bffc2681fafda28ad90ecf4a75e6af9c45f
HACHED Mhamed Amine <mhached@jems-group.com>
2025-12-01 16:33:41 +0100

    fix: Update CORS configuration to disallow credentials for enhanced security

--- src/main/java/com/bc/bissapp/config/WebConfig.java
+++ src/main/java/com/bc/bissapp/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://localhost:3000", "http://localhost:4200", "http://localhost:5173", "*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true)
+                .allowCredentials(false)
                 .maxAge(3600);
     }
 }
